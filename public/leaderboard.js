404b88aba689dcb302a3ad0cd261d23364318bbc
Jacob Harris <jacobprestonharris@gmail.com>
2024-06-10 06:52:29 +0000

    ok

--- public/leaderboard.js
+++ public/leaderboard.js
@@ -1,13 +1,4 @@
 // Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDxljmVtRsgUzDxmBUpG2DqKMO_y5ZwPdQ",
-  authDomain: "infinisweeper.firebaseapp.com",
-  projectId: "infinisweeper",
-  storageBucket: "infinisweeper.appspot.com",
-  messagingSenderId: "1032351039520",
-  appId: "1:1032351039520:web:a82823c12bca7a84ba7c45",
-  measurementId: "G-ZW6HN7WDTP",
-};
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
